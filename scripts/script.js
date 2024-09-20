15749996f57a1ddc5189411111ad9783c09ffd2a
GabrielMeloFerreira <gabskael@gmail.com>
2024-09-20 16:55:39 -0300

    Adicionado base de experiencias

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName('job3').addEventListener('click', function() {
+    const linha = document.getElementsByClassName('line');
+    linha.style.display = linha.style.display === 'block' ? 'none' : 'block';
+});
